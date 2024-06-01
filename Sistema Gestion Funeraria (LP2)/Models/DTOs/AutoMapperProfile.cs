6529bb7382e169c8b6a15dfca9e99a654f02b94d
Emmanuel G <123603722+MrNadie032@users.noreply.github.com>
2024-06-01 15:34:43 -0400

    feat: Mapeo de datos usando AutoMapper

---
+++ Sistema Gestion Funeraria (LP2)/Models/DTOs/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Sistema_Gestion_Funeraria__LP2_.Models.DTOs.Empleados;
+
+namespace Sistema_Gestion_Funeraria__LP2_.Models.DTOs
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<EmpleadoGetDTO, Empleado>().ReverseMap();
+            CreateMap<EmpleadoInsertDTO, Empleado>().ReverseMap();
+            CreateMap<EmpleadoUpdateDTO, Empleado>().ReverseMap();
+        }
+    }
+}
