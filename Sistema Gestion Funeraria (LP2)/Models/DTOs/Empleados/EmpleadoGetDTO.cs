d9cf63f15f0d883e069f17984f89dad951e34c1c
Emmanuel G <123603722+MrNadie032@users.noreply.github.com>
2024-06-01 15:34:14 -0400

    feat: Creacion de DTOs de Empleados

---
+++ Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoGetDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Sistema_Gestion_Funeraria__LP2_.Models.DTOs.Empleados
+{
+    public class EmpleadoGetDTO
+    {
+        public int IdEmpleado { get; set; }
+
+        public int? IdIdentificacion { get; set; }
+
+        public int? IdCargo { get; set; }
+        public string? NumDocumento { get; set; }
+        public string Nombre { get; set; } = null!;
+
+        public string Direccion { get; set; } = null!;
+
+        public string Telefono { get; set; } = null!;
+
+    }
+}
---
+++ Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoInsertDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema_Gestion_Funeraria__LP2_.Models.DTOs.Empleados
+{
+    public class EmpleadoInsertDTO
+    {
+        public int? IdIdentificacion { get; set; }
+
+        public int? IdCargo { get; set; }
+        public string? NumDocumento { get; set; }
+        [Required]
+        public string Nombre { get; set; } = null!;
+
+        public string Direccion { get; set; } = null!;
+
+        public string Telefono { get; set; } = null!;
+    }
+}
