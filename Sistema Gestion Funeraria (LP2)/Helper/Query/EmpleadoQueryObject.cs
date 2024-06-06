8647d8b5fc2e248ccce4ff2b608a8edfcb463cba
GuilleAQN <omarguillermo1@gmail.com>
2024-06-06 12:28:57 -0400

    feat: Añadir QueryObject para Empleados

---
+++ Sistema Gestion Funeraria (LP2)/Helper/Query/EmpleadoQueryObject.cs
@@ -0,0 +1,9 @@
+namespace Sistema_Gestion_Funeraria__LP2_.Helper.Query
+{
+    public class EmpleadoQueryObject
+    {
+        public string? NumDocumento { get; set; }
+        public string? Nombre { get; set; } = null!;
+        public string? Telefono { get; set; } = null!;
+    }
+}
