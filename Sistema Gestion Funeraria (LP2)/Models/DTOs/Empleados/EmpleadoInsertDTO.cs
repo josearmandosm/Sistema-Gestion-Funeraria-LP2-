890766e5bf3f421312487772603f7daf1e62422c
GuilleAQN <omarguillermo1@gmail.com>
2024-06-06 12:28:33 -0400

    feat: Añadir data anotations a los DTOs

--- Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoInsertDTO.cs
+++ Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoInsertDTO.cs
@@ -4,15 +4,22 @@ namespace Sistema_Gestion_Funeraria__LP2_.Models.DTOs.Empleados
 {
     public class EmpleadoInsertDTO
     {
-        public int? IdIdentificacion { get; set; }
-
-        public int? IdCargo { get; set; }
-        public string? NumDocumento { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Nombre es requerido")]
+        [MaxLength(100, ErrorMessage = "Nombre no puede ser mayor a 100 carácteres")]
         public string Nombre { get; set; } = null!;
-
+        [Required(ErrorMessage = "Número de Documento es requerido")]
+        [MinLength(9, ErrorMessage = "Número de Documento no puede ser menor a 9 carácteres")]
+        [MaxLength(11, ErrorMessage = "Número de Documento no puede ser mayor a 11 carácteres")]
+        public string? NumDocumento { get; set; }
+        [Required(ErrorMessage = "Dirección es requerida")]
+        [MaxLength(200, ErrorMessage = "Número de Documento no puede ser mayor a 200 carácteres")]
         public string Direccion { get; set; } = null!;
-
+        [Required(ErrorMessage = "Teléfono es requerido")]
+        [MaxLength(10, ErrorMessage = "Número de Documento no puede ser mayor a 10 carácteres")]
         public string Telefono { get; set; } = null!;
+        [Required(ErrorMessage = "Tipo de Identificación es requerido")]
+        public int? IdIdentificacion { get; set; }
+        [Required(ErrorMessage = "Tipo de Cargo es requerido")]
+        public int? IdCargo { get; set; }
     }
 }
--- Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoUpdateDTO.cs
+++ Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoUpdateDTO.cs
@@ -1,18 +1,27 @@
-﻿namespace Sistema_Gestion_Funeraria__LP2_.Models.DTOs.Empleados
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema_Gestion_Funeraria__LP2_.Models.DTOs.Empleados
 {
     public class EmpleadoUpdateDTO
     {
+        [Required(ErrorMessage = "Id es requerido")]
         public int IdEmpleado { get; set; }
-
-        public int? IdIdentificacion { get; set; }
-
-        public int? IdCargo { get; set; }
-        public string? NumDocumento { get; set; }
-
+        [Required(ErrorMessage = "Nombre es requerido")]
+        [MaxLength(100, ErrorMessage = "Nombre no puede ser mayor a 100 carácteres")]
         public string Nombre { get; set; } = null!;
-
+        [Required(ErrorMessage = "Número de Documento es requerido")]
+        [MinLength(9, ErrorMessage = "Número de Documento no puede ser menor a 9 carácteres")]
+        [MaxLength(11, ErrorMessage = "Número de Documento no puede ser mayor a 11 carácteres")]
+        public string? NumDocumento { get; set; }
+        [Required(ErrorMessage = "Dirección es requerida")]
+        [MaxLength(200, ErrorMessage = "Número de Documento no puede ser mayor a 200 carácteres")]
         public string Direccion { get; set; } = null!;
-
+        [Required(ErrorMessage = "Teléfono es requerido")]
+        [MaxLength(10, ErrorMessage = "Número de Documento no puede ser mayor a 10 carácteres")]
         public string Telefono { get; set; } = null!;
+        [Required(ErrorMessage = "Tipo de Identificación es requerido")]
+        public int? IdIdentificacion { get; set; }
+        [Required(ErrorMessage = "Tipo de Cargo es requerido")]
+        public int? IdCargo { get; set; }
     }
 }
