3b9a651d450fe52e0df74ffc0199998737626b6d
GuilleAQN <omarguillermo1@gmail.com>
2024-06-08 17:49:44 -0400

    fix: Mensaje de error por DataAnotattions incorrecto

--- Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoUpdateDTO.cs
+++ Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoUpdateDTO.cs
@@ -6,18 +6,19 @@ namespace Sistema_Gestion_Funeraria__LP2_.Models.DTOs.Empleados
     {
         [Required(ErrorMessage = "Id es requerido")]
         public int IdEmpleado { get; set; }
+        
         [Required(ErrorMessage = "Nombre es requerido")]
         [MaxLength(100, ErrorMessage = "Nombre no puede ser mayor a 100 carácteres")]
         public string Nombre { get; set; } = null!;
         [Required(ErrorMessage = "Número de Documento es requerido")]
-        [MinLength(9, ErrorMessage = "Número de Documento no puede ser menor a 9 carácteres")]
+        [MinLength(11, ErrorMessage = "Número de Documento no puede ser menor a 9 carácteres")]
         [MaxLength(11, ErrorMessage = "Número de Documento no puede ser mayor a 11 carácteres")]
         public string? NumDocumento { get; set; }
         [Required(ErrorMessage = "Dirección es requerida")]
-        [MaxLength(200, ErrorMessage = "Número de Documento no puede ser mayor a 200 carácteres")]
+        [MaxLength(200, ErrorMessage = "Dirección no puede ser mayor a 200 carácteres")]
         public string Direccion { get; set; } = null!;
         [Required(ErrorMessage = "Teléfono es requerido")]
-        [MaxLength(10, ErrorMessage = "Número de Documento no puede ser mayor a 10 carácteres")]
+        [MaxLength(10, ErrorMessage = "Teléfono no puede ser mayor a 10 carácteres")]
         public string Telefono { get; set; } = null!;
         [Required(ErrorMessage = "Tipo de Identificación es requerido")]
         public int? IdIdentificacion { get; set; }
