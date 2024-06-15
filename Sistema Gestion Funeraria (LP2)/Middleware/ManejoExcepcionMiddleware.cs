d9a6051613b76ff913f238ce0f6db2ca3f7bc416
GuilleAQN <omarguillermo1@gmail.com>
2024-06-15 15:02:11 -0400

    refactor: Renombre de Middleware de Excepciones

---
+++ Sistema Gestion Funeraria (LP2)/Middleware/ManejoExcepcionMiddleware.cs
@@ -0,0 +1,44 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+public class ManejadorExcepcionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ManejadorExcepcionMiddleware> _logger;
+
+    public ManejadorExcepcionMiddleware(RequestDelegate next, ILogger<ManejadorExcepcionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Una excepción no manejada ha ocurrido.");
+            await ManejarExcepcionAsync(context, ex);
+        }
+    }
+
+    private static Task ManejarExcepcionAsync(HttpContext context, Exception exception)
+    {
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Un error inesperado ha ocurrido!",
+            Detail = exception.Message,
+            Instance = context.Request.Path
+        };
+
+        context.Response.ContentType = "application/problem+json";
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        var json = JsonSerializer.Serialize(problemDetails);
+        return context.Response.WriteAsync(json);
+    }
+}
