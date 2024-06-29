158d5efe3a58d7967c2a6a0ef7d500f536e472f6
GuilleAQN <omarguillermo1@gmail.com>
2024-06-29 10:10:39 -0400

    refactor: Actualizar DTOs de Empleados con el modelos Normalizado

--- Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoGetDTO.cs
+++ Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoGetDTO.cs
@@ -5,11 +5,21 @@ namespace Sistema_Gestion_Funeraria__LP2_.Models.DTOs.Empleados
     public class EmpleadoGetDTO
     {
         public int IdEmpleado { get; set; }
+
+        public string NumDocumento { get; set; } = null!;
+
+        public int IdTipoIdentificacion { get; set; }
+
         public string Nombre { get; set; } = null!;
-        public string? NumDocumento { get; set; }
-        public string Direccion { get; set; } = null!;
+
+        public string? Direccion { get; set; }
+
         public string Telefono { get; set; } = null!;
-        public int? IdIdentificacion { get; set; }
-        public int? IdCargo { get; set; }
+
+        public int IdCargo { get; set; }
+
+        public int IdJornadaLaboral { get; set; }
+
+        public int IdLocalidad { get; set; }
     }
 }
--- Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoInsertDTO.cs
+++ Sistema Gestion Funeraria (LP2)/Models/DTOs/Empleados/EmpleadoInsertDTO.cs
@@ -7,19 +7,30 @@ namespace Sistema_Gestion_Funeraria__LP2_.Models.DTOs.Empleados
         [Required(ErrorMessage = "Nombre es requerido")]
         [MaxLength(100, ErrorMessage = "Nombre no puede ser mayor a 100 carácteres")]
         public string Nombre { get; set; } = null!;
+
         [Required(ErrorMessage = "Número de Documento es requerido")]
         [MinLength(11, ErrorMessage = "Número de Documento no puede ser menor a 9 carácteres")]
         [MaxLength(11, ErrorMessage = "Número de Documento no puede ser mayor a 11 carácteres")]
         public string? NumDocumento { get; set; }
+
         [Required(ErrorMessage = "Dirección es requerida")]
         [MaxLength(200, ErrorMessage = "Dirección no puede ser mayor a 200 carácteres")]
         public string Direccion { get; set; } = null!;
+
         [Required(ErrorMessage = "Teléfono es requerido")]
         [MaxLength(10, ErrorMessage = "Teléfono no puede ser mayor a 10 carácteres")]
         public string Telefono { get; set; } = null!;
+
         [Required(ErrorMessage = "Tipo de Identificación es requerido")]
-        public int? IdIdentificacion { get; set; }
+        public int? IdTipoIdentificacion { get; set; }
+
         [Required(ErrorMessage = "Tipo de Cargo es requerido")]
         public int? IdCargo { get; set; }
+
+        [Required(ErrorMessage = "La Jornada Laboral asociada requerida")]
+        public int? IdJornadaLaboral { get; set; }
+
+        [Required(ErrorMessage = "La Localidad asociada requerida")]
+        public int? IdLocalidad { get; set; }
     }
 }
